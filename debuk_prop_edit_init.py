fb513713dabc032cfd75a275d0756d58145bdd3e
debukgit <32515367+debukgit@users.noreply.github.com>
2022-04-03 15:08:47 +0200

    Add files via upload

--- debuk_prop_edit_init.py
+++ debuk_prop_edit_init.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "Debuk PropEditInit",
     "description": "Initiates Proportional Editing Radius",
     "author": "Debuk",
-    "version": (1, 0, 0),
+    "version": (1, 0, 1),
     'license': 'GPL v3',
     "blender": (2, 80, 0),
     "support": "COMMUNITY",
@@ -10,17 +10,41 @@ bl_info = {
 }
 
 import bpy
+from bpy.types import AddonPreferences
 from bpy.app.handlers import persistent
+from bpy.props import FloatProperty
 
+class DebukPropEditInitAddonPreferences(AddonPreferences):
+    bl_idname = __name__
+    
+    
+    def rad_upd(self, context):
+        bpy.context.scene.tool_settings.proportional_size = self.prop_edit_radius
+    
+    prop_edit_radius: FloatProperty(
+        name ="Prop Edit radius",
+        default=0.1,
+        min = 0.01,
+        max = 10,
+        precision=2,
+        update=rad_upd
+    )
 
+    def draw(self, context):
+        layout = self.layout
+        layout.prop(self, "prop_edit_radius")
+
+        
 @persistent
 def load_handler(dummy):
-    bpy.context.scene.tool_settings.proportional_size = 0.1
 
+    addon_prefs = bpy.context.preferences.addons["debuk_prop_edit_init"].preferences
+    bpy.context.scene.tool_settings.proportional_size = addon_prefs.prop_edit_radius
 
 def register():
+    bpy.utils.register_class(DebukPropEditInitAddonPreferences)
     bpy.app.handlers.load_post.append(load_handler) 
-
 
 def unregister():
     bpy.app.handlers.load_post.remove(load_handler)
+    bpy.utils.register_class(DebukPropEditInitAddonPreferences)
