6ea9366d684ece6272f86c3245aea90cb8904785
debukgit <32515367+debukgit@users.noreply.github.com>
2022-04-03 09:57:31 +0200

    Add files via upload

---
+++ debuk_propedit_init.py
@@ -0,0 +1,26 @@
+bl_info = {
+    "name": "Debuk PropEdit-Init",
+    "description": "Initiates Proportional Editing Radius",
+    "author": "Debuk",
+    "version": (1, 0, 0),
+    'license': 'GPL v3',
+    "blender": (2, 80, 0),
+    "support": "COMMUNITY",
+    "category": "Object"
+}
+
+import bpy
+from bpy.app.handlers import persistent
+
+
+@persistent
+def load_handler(dummy):
+    bpy.context.scene.tool_settings.proportional_size = 0.1
+
+
+def register():
+    bpy.app.handlers.load_post.append(load_handler) 
+
+
+def unregister():
+    bpy.app.handlers.load_post.remove(load_handler)
